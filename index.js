628c56a033d4428ae411c0ccb2f11fb95bc2f29b
hiskiabororing <69854351+hiskiabororing@users.noreply.github.com>
2020-12-08 15:45:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+<input id="print-area" type="text" value="20" />
+// js
+let print = document.getElementById("print-area").value;
+
+console.log(print-area); // Output: 20
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href 
+	var tujuan = $(this).attr('href');
+	// tangkap elemen yang bersangkutan
+	var elemenTujuan = $(tujuan);
+});
+
